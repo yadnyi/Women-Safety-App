f05c2b27b78d978128ef7aed1d4a2f03605f9e17
Yadnyi <deshpandeyadnyi@gmail.com>
2021-07-20 10:31:11 +0530

    Added Login Page

---
+++ lib/Login.dart
@@ -0,0 +1,135 @@
+import 'package:aaryaa/Location.dart';
+import 'package:aaryaa/Registration.dart';
+import 'package:aaryaa/TabBarPage.dart';
+import 'package:aaryaa/sms.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'HomePage.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _auth = FirebaseAuth.instance;
+  bool showProgress = false;
+  late String email, password;
+  late String name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(
+        child: ModalProgressHUD(
+          inAsyncCall: showProgress,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 40.0,
+              ),
+              Image.asset("Aaryaa.png", width: 200.0,),
+              SizedBox(
+                height: 40.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                //textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value; // get value from TextField
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter your Email",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                obscureText: true,
+                //textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value; //get value from textField
+                },
+                decoration: InputDecoration(
+                    hintText: "Enter your Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Material(
+                elevation: 5,
+                color: Color(0xffbe3a5a),
+                borderRadius: BorderRadius.circular(32.0),
+                child: MaterialButton(
+                  onPressed: () async {
+                    setState(() {
+                      showProgress = true;
+                    });
+                    try {
+                      final newUser = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      print(newUser.toString());
+                      if (newUser != null) {
+                        Fluttertoast.showToast(
+                            msg: "Login Successfull",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.blueAccent,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TabBarPage()),
+                        );
+                        setState(() {
+                          showProgress = false;
+                        });
+                      }
+                    } catch (e) {}
+                  },
+                  minWidth: 200.0,
+                  height: 45.0,
+                  child: Text(
+                    "Login",
+                    style:
+                    TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Registration()),
+                  );
+                },
+                child: Text(
+                  "New User ?",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
